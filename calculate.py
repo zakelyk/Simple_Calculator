57762f951d556cc8449c342375e321c4991bdc6d
zakelyk <candra.setyo.w@gmail.com>
2024-01-05 13:48:27 +0700

    belum beres

--- calculate.py
+++ calculate.py
@@ -1,18 +1,43 @@
-def format_number(num):
-    if num % 1 == 0:
-        return int(num)
-    else:
-        return num
+# def format_number(num):
+#     if num % 1 == 0:
+#         return int(num)
+#     else:
+#         return num
 
-def calculate(operand1, operand2, operator):
-    if operator == "+":
-        return format_number(operand1 + operand2)
-    elif operator == "-":
-        return format_number(operand1 - operand2)
-    elif operator == "*":
-        return format_number(operand1 * operand2)
-    elif operator == "/":
-        if operand2 == 0:
-            return "Error"
+# def calculate(operand1, operand2, operator):
+#     if operator == "+":
+#         return format_number(operand1 + operand2)
+#     elif operator == "-":
+#         return format_number(operand1 - operand2)
+#     elif operator == "*":
+#         return format_number(operand1 * operand2)
+#     elif operator == "/":
+#         if operand2 == 0:
+#             return "Error"
+#         else:
+#             return format_number(operand1 / operand2)
+
+
+def calculate_decorator(func):
+    def wrapper(a, b, operator):
+        # print(f"Calculating {a} {operator} {b}")
+        result = func(a, b, operator)
+        # print(f"Result: {result}")
+        return result
+    return wrapper
+
+@calculate_decorator
+def calculate(a, b, operator):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        if b != 0:
+            return a / b
         else:
-            return format_number(operand1 / operand2)
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
--- control.py
+++ control.py
@@ -1,5 +1,6 @@
 import flet
-from calculate import calculate, format_number
+# from calculate import calculate, format_number\
+import calculate
 from flet import (
     Column,
     Container,
@@ -202,57 +203,62 @@ class CalculatorApp(UserControl):
                 ],
             ),
         )
-
+    
     def button_clicked(self, e):
-        data = e.control.data
+        data = e.controls.data
         if self.result.value == "Error" or data == "AC":
             self.result.value = "0"
             self.reset()
-
+        
         elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
             if self.result.value == "0" or self.new_operand == True:
                 self.result.value = data
                 self.new_operand = False
             else:
                 self.result.value = self.result.value + data
-
         elif data in ("+", "-", "*", "/"):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
-            self.operator = data
-            if self.result.value == "Error":
-                self.operand1 = "0"
-            else:
-                self.operand1 = float(self.result.value)
-            self.new_operand = True
-
-        elif data in ("="):
-            self.result.value = self.calculate(
-                self.operand1, float(self.result.value), self.operator
-            )
+            self.result.value = calculate(self.operand1, float(self.result.value), data)
             self.reset()
+            
+            
 
-        elif data in ("%"):
-            self.result.value = float(self.result.value) / 100
-            self.reset()
+        # elif data in ("+", "-", "*", "/"):
+        #     self.result.value = self.calculate(
+        #         self.operand1, float(self.result.value), self.operator
+        #     )
+        #     self.operator = data
+        #     if self.result.value == "Error":
+        #         self.operand1 = "0"
+        #     else:
+        #         self.operand1 = float(self.result.value)
+        #     self.new_operand = True
 
-        elif data in ("+/-"):
-            if float(self.result.value) > 0:
-                self.result.value = "-" + str(self.result.value)
+    #     elif data in ("="):
+    #         self.result.value = self.calculate(
+    #             self.operand1, float(self.result.value), self.operator
+    #         )
+    #         self.reset()
 
-            elif float(self.result.value) < 0:
-                self.result.value = str(
-                    self.format_number(abs(float(self.result.value)))
-                )
+    #     elif data in ("%"):
+    #         self.result.value = float(self.result.value) / 100
+    #         self.reset()
 
-        self.update()
+    #     elif data in ("+/-"):
+    #         if float(self.result.value) > 0:
+    #             self.result.value = "-" + str(self.result.value)
 
-    def format_number(self, num):
-        return format_number(num)    
+    #         elif float(self.result.value) < 0:
+    #             self.result.value = str(
+    #                 self.format_number(abs(float(self.result.value)))
+    #             )
 
-    def calculate(self, operand1, operand2, operator):
-        return calculate(operand1, operand2, operator)
+    #     self.update()
+
+    # def format_number(self, num):
+    #     return format_number(num)    
+
+    # def calculate(self, operand1, operand2, operator):
+    #     return calculate(operand1, operand2, operator)
 
     def reset(self):
         self.operator = "+"
